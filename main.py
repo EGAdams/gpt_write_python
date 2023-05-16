77d295206b6bea6dac2ed88b0b7fcf59db7eb9fb
egadams <eg1972@gmail.com>
2023-05-16 12:57:11 -0400

    works on chromebook

--- main.py
+++ main.py
@@ -37,6 +37,24 @@ async def write_code():
 # async def run_code():
     # return await CodeRunner.run_code()
 
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
