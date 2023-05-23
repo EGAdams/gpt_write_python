d908a4191e9194af89f8dc412159a5d9275a7c5a
egadams <eg1972@gmail.com>
2023-05-23 04:06:48 -0400

    add ability to execute Python code.

---
+++ agent_test/test_agent.py
@@ -0,0 +1,17 @@
+import unittest
+from langchain.agents.agent import BaseSingleActionAgent
+
+
+class TestBaseSingleActionAgent(unittest.TestCase):
+    def setUp(self):
+        self.agent = BaseSingleActionAgent()
+
+    def test_return_values(self):
+        self.assertEqual(self.agent.return_values, ['output'])
+
+    def test_get_allowed_tools(self):
+        self.assertIsNone(self.agent.get_allowed_tools())
+
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -1,105 +1,35 @@
-# 
-# Main Python file: main.py 
-
-# Description:
-# This file is the entry point for your plugin.
-
-# It contains the following methods:
-#   * execute_python_file - Accepts a filename and returns a JSON object.
-#   * write_code - Accepts a code string and writes it to a file.
-# 
-# ``` python
 import os
 import subprocess
 import tempfile
 import random
+import logging_code
 
 from quart import request, Response, Quart, jsonify
 import quart_cors
 import quart
-# from code_runner import CodeRunner  # assuming the class is in a file named code_runner.py
 
-
-from base_tool import BaseTool
-from python_repl import PythonREPL
-from python_repl_tool import PythonREPLTool
-from python_ast_repl_tool import PythonAstREPLTool
-
-from python_file_executor import PythonFileExecutor
+from PythonCodeRunner import PythonCodeRunner
+from PythonAstCodeRunner import PythonAstCodeRunner
+from CodeWriter import CodeWriter
+from FileReader import FileReader
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 @app.post("/run_python_code")
 async def run_python_code():
-    request_data = await quart.request.get_json(force=True)
-    code = request_data.get("code")
-    tool = PythonREPLTool()
-    result = tool._run(code)
-    return Response(response=result, status=200)
+    return await PythonCodeRunner.run_python_code()
 
 @app.post("/run_python_ast_code")
 async def run_python_ast_code():
-    request_data = await quart.request.get_json(force=True)
-    code = request_data.get("code")
-    tool = PythonAstREPLTool()
-    result = tool._run(code)
-    return Response(response=result, status=200)
+    return await PythonAstCodeRunner.run_python_ast_code()
 
 @app.post("/write_code")
 async def write_code():
-    request_data = await quart.request.get_json(force=True)
-    code = request_data.get("code")
-    directory = request_data.get("directory")
-    filename = request_data.get("filename", "code.py")
-
-    if not code:
-        return Response(response='No code provided', status=400)
-
-    if not directory:
-        directory = os.path.join(tempfile.gettempdir(), str(random.randint(1000, 9999)))
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    file_path = os.path.join(directory, filename)
-
-    with open(file_path, 'w') as file:
-        file.write(code)
-
-    return Response(response=f'Code written to {file_path}', status=200)
+    return await CodeWriter.write_code()
 
 @app.post("/read_file")
 async def read_file():
-    request_data = await quart.request.get_json(force=True)
-    filepath = request_data.get("filepath")
-
-    if not filepath:
-        return Response(response='No filepath provided', status=400)
-
-    if not os.path.exists(filepath):
-        return Response(response=f'File {filepath} does not exist', status=404)
-
-    with open(filepath, 'r') as file:
-        content = file.read()
-
-    return Response(response=content, status=200)
-
-
-
-
-# app = Quart(__name__)
-# executor = PythonFileExecutor()
-
-# @app.route('/execute_python_file', methods=['POST'])
-# async def execute_python_file():
-#     data = await request.get_json()
-#     filename = data.get('filename')
-#     result = executor.execute_python_file(filename)
-#     return jsonify(result)
-
-# @app.post("/run_code")
-# async def run_code():
-    # return await CodeRunner.run_code()
+    return await FileReader.read_file()
 
 @app.get("/logo.png")
 async def plugin_logo():
@@ -125,4 +55,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-#```
